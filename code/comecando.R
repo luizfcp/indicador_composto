9642ec738c06ecc22f295b2f793e1fae068f2dbd
luizfcp <luizfcpassos@hotmail.com>
2019-05-07 21:50:34 +0000

    code

--- code/comecando.R
+++ code/comecando.R
@@ -1,25 +1,92 @@
-# library(readxl)
-library(readr)
+
 library(dplyr)
 library(tidyr)
+library(purrr)
 library(magrittr)
 
-# base <- read_csv2(paste0("../data/", list.files("../data/")[8]))
-base <- read.csv2(paste0("../data/", list.files("../data/")[8]), encoding = "UTF-8")
-base %>% View
 
-base %>% 
-  select(Cod.IBGE, Municipio, Natureza, Registros, Mes, Ano) %>% 
-  as_tibble() %>% 
-  nest(-Ano) %$% 
-  data %>% 
-  .[[1]] %>% 
-  nest(-Municipio) %$% 
-  data %>%  
-  .[[1]] %>% 
-  group_by(Natureza) %>% 
-  summarise(sum = sum(Registros)) %>% 
-  View
+base_alvos_de_roubo <- readRDS("../data/rds/Banco Alvos de Roubo 2019 - Por Municpio e Por RISP.rds")
+base_crimes_violentos <- readRDS("../data/rds/Banco Crimes Violentos Armazm 2019 - Por municipio e Por RISP.rds")
+base_outras_naturezas <- readRDS("../data/rds/Banco Outras Naturezas 2019 - Por Municpio e Por RISP.rds")
+base_veiculos_roubados <- readRDS("../data/rds/Banco Veculos Roubados 2019 - Por Municpio e Por RISP.rds")
+base_vitimas_homicidios <- readRDS("../data/rds/Banco Vtimas de Homicdio Consumado 2019 - Por Municpio e Por RISP.rds")
+
+
+data <- 
+  base_crimes_violentos %>% 
+  select(Município, Natureza, Registros, Ano) %>% #`Cod. IBGE`, 
+  nest(-Ano) %>% 
+  mutate(
+    data = map(
+      data,
+      ~ .x %>% 
+        group_by(Município, Natureza) %>% 
+        summarise(registro_anual_crimes_violentos = sum(Registros)) %>%
+        spread(Natureza, registro_anual_crimes_violentos)
+    )
+  ) %>% 
+  unnest() %>% 
+  full_join(
+    base_outras_naturezas %>% 
+      select(Município, Natureza, Registros, Ano) %>% #`Cod. IBGE`, 
+      nest(-Ano) %>% 
+      mutate(
+        data = map(
+          data,
+          ~ .x %>% 
+            group_by(Município, Natureza) %>% 
+            summarise(registro_anual_outras_naturezas = sum(Registros)) %>%
+            spread(Natureza, registro_anual_outras_naturezas)
+        )
+      ) %>% 
+      unnest()
+  ) %>% 
+  full_join(
+    base_vitimas_homicidios %>% 
+      select(Município, Natureza, Vítimas, Ano) %>% #`Cod. IBGE`, 
+      nest(-Ano) %>% 
+      mutate(
+        data = map(
+          data,
+          ~ .x %>% 
+            group_by(Município, Natureza) %>% 
+            summarise(registro_anual_vitimas_homicidios = sum(Vítimas)) %>%
+            spread(Natureza, registro_anual_vitimas_homicidios)
+        )
+      ) %>% 
+      unnest()
+  )
+
+    
+## a partir de 2015 apenas
+base_alvos_de_roubo %>% 
+  select(Município, Natureza, Registros, Ano) %>% #`Cod. IBGE`, 
+  nest(-Ano) %>% 
+  mutate(
+    data = map(
+      data,
+      ~ .x %>% 
+        group_by(Município, Natureza) %>% 
+        summarise(registro_anual_alvos_de_roubo = sum(Registros)) %>%
+        spread(Natureza, registro_anual_alvos_de_roubo)
+    )
+  ) %>% 
+  unnest()
+  
+base_veiculos_roubados %>% 
+  select(Município, Natureza, Registros, Ano) %>% #`Cod. IBGE`, 
+  nest(-Ano) %>% 
+  mutate(
+    data = map(
+      data,
+      ~ .x %>% 
+        group_by(Município, Natureza) %>% 
+        summarise(registro_anual_veiculos_roubados = sum(Registros)) %>%
+        spread(Natureza, registro_anual_veiculos_roubados)
+    )
+  ) %>% 
+  unnest()
+
 
 ## Agregar todos os tipos de estupros em estupro e Homicidios em homicidios
 
