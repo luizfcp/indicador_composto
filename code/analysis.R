4ec9837d86eea491bffd5b975336f91abd370cf8
luizfcp <luizfcpassos@hotmail.com>
2019-05-19 05:10:49 +0000

    analysis

--- code/analysis.R
+++ code/analysis.R
@@ -8,6 +8,8 @@ library(magrittr)
 library(readxl)
 library(janitor)
 library(GGally)
+library(stringr)
+library(abjutils)
 
 options(scipen = 99999999)
 
@@ -16,12 +18,16 @@ options(scipen = 99999999)
 base_crimes_violentos   <- readRDS("../data/rds/Banco Crimes Violentos Armazm 2019 - Por municipio e Por RISP.rds")     %>% clean_names()
 base_outras_naturezas   <- readRDS("../data/rds/Banco Outras Naturezas 2019 - Por Municpio e Por RISP.rds")             %>% clean_names()
 base_vitimas_homicidios <- readRDS("../data/rds/Banco Vtimas de Homicdio Consumado 2019 - Por Municpio e Por RISP.rds") %>% clean_names()
+base_internacoes        <- readRDS("../data/rds/internacoes.rds") %>% clean_names()
+base_obitos             <- readRDS("../data/rds/obitos.rds") %>% clean_names()
+base_pib_per_capita     <- readRDS("../data/rds/pib_per_capita.rds") %>% clean_names()
+base_pib_per_corrente   <- readRDS("../data/rds/pib_per_corrente.rds") %>% clean_names()
 
-populacao <- read_excel("../data/2019-04-05 - Banco Outras Naturezas Armazm - Atualizado Maro 2019 - Por Municpio e Por RISP.xlsx",
-                        sheet = 5)
+populacao <- read_excel("../data/2019-04-05 - Banco Outras Naturezas Armazm - Atualizado Maro 2019 - Por Municpio e Por RISP.xlsx", sheet = 5)
 
 # Wrangling ---------------------------------------------------------------
 
+# Base de dados populacao por municipio
 pop_mg_municipios <- 
   populacao %>%
   clean_names() %>%
@@ -33,7 +39,8 @@ pop_mg_municipios <-
   gather(ano, populacao, -cod_ibge) %>% 
   mutate(ano = as.numeric(ano))
 
-raw_data <- 
+# Base de dados primaria
+bases_prim <- 
   # Crimes Violentos
   base_crimes_violentos %>% 
   unite(municipio, c(cod_ibge, municipio), sep = "_") %>% 
@@ -84,11 +91,49 @@ raw_data <-
       unnest()
   ) %>% 
   clean_names() %>% 
+  separate(municipio, c("cod_ibge", "municipio"), sep = "_") %>% 
   filter(ano %in% 2013:2016)
+
+# Base de dados secundaria
+bases_sec <- 
+  # Internacoes
+  base_internacoes %>% 
+  mutate(
+    municipio = base_internacoes$municipio %>% 
+      unlist() %>% 
+      .[-(base_internacoes$municipio %>% unlist() %>% str_detect("^0${1}") %>% which)]
+  ) %>% 
+  filter(ano %in% 2013:2016) %>% 
+  arrange(ano, municipio) %>% 
+  mutate(municipio = bases_prim$cod_ibge) %>% 
+  full_join(
+    # Obitos
+    base_obitos %>% 
+      filter(ano %in% 2013:2016) %>% 
+      mutate(municipio = bases_prim$cod_ibge)
+  ) %>% 
+  full_join(
+    # PIB per capita
+    base_pib_per_capita %>% 
+      select(-municipio) %>%
+      filter(ano %in% 2013:2016) %>% 
+      mutate(codigo = codigo %>% str_sub(end = 6)) %>% 
+      rename("municipio"="codigo")
+  ) %>% 
+  full_join(
+    # PIB per corrente
+    base_pib_per_corrente %>% 
+      select(-municipio) %>%
+      filter(ano %in% 2013:2016) %>% 
+      mutate(codigo = codigo %>% str_sub(end = 6)) %>% 
+      rename("municipio"="codigo")
+  ) %>% 
+  mutate(ano = as.numeric(ano)) %>% 
+  `colnames<-`(c("cod_ibge", "ano", "internacoes", "obitos", "pib_per_capita", "pib_per_corrente"))
 
 
 data <- 
-  raw_data %>% 
+  bases_prim %>% 
   mutate(
     # Agregando os tipos de Homicidios (tentado, consumado e vitimas de homicidios consumado)
     homicidios = homicidio_tentado+homicidio_consumado_registros+vitima_de_homicidio_consumado,
@@ -102,21 +147,25 @@ data <-
     # Agregando roubo e furto
     assalto = roubo_consumado+furto_consumado
   ) %>% 
-  select(ano, municipio, assalto, estupro, homicidios, extorsao, lesao_corporal_consumado) %>% 
-  separate(municipio, c("cod_ibge", "municipio"), sep = "_")
+  select(ano, cod_ibge, municipio, assalto, estupro, homicidios, extorsao, lesao_corporal_consumado) %>% 
+  full_join(bases_sec)
 
 
 # Proporcao dos dados (crime/populacao)
 data_prop <- 
-  full_join(data, pop_mg_municipios, by = c("ano", "cod_ibge")) %>% 
+  full_join(data, pop_mg_municipios) %>% 
   nest(-ano) %>% 
   mutate(data_prop = map(data,
-                         ~ .x %<>% mutate(
-                           prop_assalto = .x$assalto/.x$populacao,
-                           prop_estupro = .x$estupro/.x$populacao,
-                           prop_homicidios = .x$homicidios/.x$populacao,
-                           prop_extorsao = .x$extorsao/.x$populacao,
-                           prop_lesao_corp = .x$lesao_corporal_consumado/.x$populacao
+                         ~ .x %>% mutate(
+                           prop_assalto     = .x$assalto/.x$populacao,
+                           prop_estupro     = .x$estupro/.x$populacao,
+                           prop_homicidios  = .x$homicidios/.x$populacao,
+                           prop_extorsao    = .x$extorsao/.x$populacao,
+                           prop_lesao_corp  = .x$lesao_corporal_consumado/.x$populacao,
+                           prop_internacoes = .x$internacoes/.x$populacao,
+                           prop_obitos      = .x$obitos/.x$populacao,
+                           prop_pib_per_cap = .x$pib_per_capita/.x$populacao,
+                           prop_pib_per_cor = .x$pib_per_corrente/.x$populacao
                          ))) %>% 
   select(ano, data_prop)
 
